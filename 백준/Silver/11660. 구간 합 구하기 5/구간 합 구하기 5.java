b8dad1900d4329aa17927b346e5c53b51b7f82a8
Hoshi03 <ichanho114@gmail.com>
2024-03-17 01:56:02 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2716 ms, Memory: 328816 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,34 +1,43 @@
+import java.io.*;
 import java.util.*;
-class Main {
 
-    public static void main(String[] args) {
-        Main T = new Main();
-        Scanner in =  new Scanner(System.in);
+class Main
+{
+    public static void main (String[] args) throws IOException {
+
+        Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int m = in.nextInt();
+        int tmp = m;
+
         int[][] arr = new int[n+1][n+1];
+        int[][] sum = new int[n+1][n+1];
+
+        int[] res = new int[m];
+
         for (int i = 1; i <= n; i++){
             for (int j = 1; j <= n; j++){
                 arr[i][j] = in.nextInt();
             }
         }
-        int[] res = new int[m];
-        int[][] sum = new int[n+1][n+1];
 
-        //구간합 구하기
-        for (int i = 1; i <= n; i++){
-            for (int j = 1; j <= n; j++){
+        for (int i = 1; i <= n; i++) {
+            sum[i][1] = sum[i-1][1] + arr[i][1];
+            sum[1][i] = sum[1][i-1] + arr[1][i];
+        }
+
+        for (int i = 2; i <= n; i++){
+            for (int j = 2; j <= n; j++){
                 sum[i][j] = sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1] + arr[i][j];
             }
         }
 
-        // 구간합 구하기 (x1,y1) (x2,y2)가 있을때를 그림으로 그려보면 이해가 된다
-        for (int i = 0; i < m; i++){
+        while (m-- > 0){
             int x1 = in.nextInt();
             int y1 = in.nextInt();
             int x2 = in.nextInt();
             int y2 = in.nextInt();
-            res[i] = sum[x2][y2] - sum[x1-1][y2] - sum[x2][y1-1] + sum[x1-1][y1-1];
+            res[tmp - m-1] = sum[x2][y2] - sum[x1-1][y2] - sum[x2][y1-1] + sum[x1-1][y1-1];
         }
 
         for (int x : res) System.out.println(x);
