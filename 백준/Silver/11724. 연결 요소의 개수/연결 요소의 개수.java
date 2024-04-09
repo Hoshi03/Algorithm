99f074357c184668932a0f3787ba425e1280e31b
Hoshi03 <ichanho114@gmail.com>
2024-04-09 15:54:05 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1604 ms, Memory: 318108 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -8,21 +8,26 @@ public class Main {
     static int[][] arr;
     static boolean[] visited;
 
-    static void dfs(int x){
-        visited[x] = true;
-
+    static void solution(){
         for (int i = 1; i <= N; i++){
-            if (arr[x][i] == 0) continue;
-            if (visited[i]) continue;
-            dfs(i);
+            if (!visited[i]) {
+                bfs(i);
+                res++;
+            }
         }
     }
 
-    static void solution(){
-        for (int i = 1; i <= N; i++){
-            if (!visited[i]){
-                res++;
-                dfs(i);
+    static void bfs(int x){
+        Queue<Integer> que = new LinkedList<>();
+        que.add(x);
+        visited[x] = true;
+        while (!que.isEmpty()){
+            int tmp = que.poll();
+            for (int i = 1; i <= N; i++){
+                if (visited[i]) continue;
+                if (arr[tmp][i] != 1) continue;
+                que.add(i);
+                visited[i] = true;
             }
         }
     }
@@ -31,6 +36,7 @@ public class Main {
         Scanner in =new Scanner(System.in);
         N = in.nextInt();
         M = in.nextInt();
+
         arr = new int[N+1][N+1];
         visited = new boolean[N+1];
         for (int i = 1; i <= M; i++) {
