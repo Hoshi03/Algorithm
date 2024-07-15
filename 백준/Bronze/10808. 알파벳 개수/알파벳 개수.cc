8ad31689447e8d22cb57e58a3695f11806fc396a
Hoshi03 <ichanho114@gmail.com>
2024-07-15 13:55:35 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(void) {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int alphabet[30] ={};
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.length(); i++) {
+        alphabet[s[i]-97]++;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        cout << alphabet[i] << " ";
+    }
+}
