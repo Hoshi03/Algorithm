445e0f604c5171cad9b03be2d2fb903557892e18
Hoshi03 <ichanho114@gmail.com>
2024-09-30 19:18:59 +0900

    [level 1] Title: 폰켓몬, Time: 1.16 ms, Memory: 4.25 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int solution(vector<int> nums)
 {
     int answer = 0, count = nums.size()/2;
-    unordered_set<int> sset;
+    set<int> sset;
     for(int x : nums)sset.insert(x);
     if(count >= sset.size()) answer = sset.size();
     else answer = count;
