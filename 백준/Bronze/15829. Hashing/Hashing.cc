c4ee77facd20b940de0a5753e9130842f5b209c0
Hoshi03 <ichanho114@gmail.com>
2024-11-09 11:27:00 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -7,13 +7,13 @@ int main() {
     int n;
     string s;
     cin >> n >> s;
-    int res = 0;
-    int ppow = 1;
+    long long res = 0;
+    long long ppow = 1;
 
     for(int i = 0; i < n; i++){
         int tmp = s[i] - 96;
-        res += tmp * ppow;
-        ppow *= 31;
+        res = (res + (tmp * ppow)) % 1234567891;
+        ppow = (ppow * 31) % 1234567891;
     }
     cout << res;
 }
