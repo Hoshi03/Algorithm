4b78ba2678428c06580ba944db16590a2925b01b
Hoshi03 <ichanho114@gmail.com>
2024-11-11 12:27:34 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 88 ms, Memory: 5700 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool sorting(pair<int, int> x1, pair<int, int> x2){
+    if(x1.second == x2.second){
+        return x1.first < x2.first;
+    }
+
+    return x1.second < x2.second;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> arr(n);
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        arr[i] = {x, y};
+    }
+
+    sort(arr.begin(), arr.end(), sorting);
+
+    string s = "";
+    for(auto x : arr) s += to_string(x.first) + " " + to_string(x.second) + '\n';
+    cout << s;
+}
+
