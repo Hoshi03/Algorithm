f2357966b7d167f8ffa30a940cfe8773bcadf3e7
Hoshi03 <ichanho114@gmail.com>
2024-11-18 13:06:01 +0900

    [Silver V] Title: 단어 정렬, Time: 520 ms, Memory: 26236 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,43 +1,31 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-
-class Words implements Comparable<Words>{
-    String word;
-
-    Words(String word){
-        this.word = word;
-    }
-
-    @Override
-    public int compareTo(Words o) {
-        if (o.word.length() == word.length()){
-            return word.compareTo(o.word);
-        }
-
-        else return word.length() - o.word.length();
-    }
-}
 
 
 public class Main {
-
-    public static void solution(char[][] arr, String s)  {
-
-    }
-
     public static void main(String[] args) throws IOException {
-
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        in.nextLine();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<String> set = new HashSet<>();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
 
-        TreeSet<Words> tr = new TreeSet<>();
-        for (int i = 0; i < n; i++) tr.add(new Words(in.nextLine()));
-
-        ArrayList<Words> arr = new ArrayList<>(tr);
-
-        Collections.sort(arr);
-
-        for (Words x : arr) System.out.println(x.word);
+        ArrayList<String> arr = new ArrayList<>(set);
+        Collections.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()){
+                    return o1.compareTo(o2);
+                }
+                return o1.length() - o2.length();
+            }
+        });
+        for(var x : arr){
+            System.out.println(x);
+        }
     }
 }
