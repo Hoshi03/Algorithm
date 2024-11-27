652bd2d4740a3a651f77bf9df57a69da55b0652c
Hoshi03 <ichanho114@gmail.com>
2024-11-27 12:22:56 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 108 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -1,22 +1,18 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static String Solution(String s){
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = br.readLine();
         char[] arr = s.toCharArray();
         for (int i = 0; i < arr.length; i++){
-            if (arr[i] >= 65 && arr[i] <= 90 ) arr[i] += 32;
-            else if (arr[i] >= 97 && arr[i] <= 122) arr[i] -= 32;
+            if (arr[i] >= 97) arr[i] -= 32;
+            else arr[i] += 32;
         }
 
-        return String.valueOf(arr);
+        for (char x : arr) System.out.print(x);
 
-    }
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        String s = in.nextLine();
-        System.out.println(Solution(s));
     }
 }
