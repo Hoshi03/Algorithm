3ae5e2ca2aaf22f91cb6afd3c5d1d30dbba1b2ff
Hoshi03 <ichanho114@gmail.com>
2024-11-27 12:27:33 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,29 +1,31 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] arr = new int[30];
 
-    public static int Solution(String s1, String s2){
-        int[] arr1 = new int[26];
-        int[] arr2 = new int[26];
+        String s1,s2;
+        s1 = br.readLine();
+        s2 = br.readLine();
+
+        for (char x : s1.toCharArray()){
+            arr[x - 97]++;
+        }
+
+        for (char x : s2.toCharArray()){
+            arr[x - 97]--;
+        }
 
         int res = 0;
 
-        for (char x : s1.toCharArray()) arr1[x - 97] += 1;
-        for (char x : s2.toCharArray()) arr2[x - 97] += 1;
-
-        for(int i = 0; i < arr1.length; i++){
-            res += Math.abs(arr1[i] - arr2[i]);
+        for (int x : arr){
+            res += Math.abs(x);
         }
 
-        return res;
+        System.out.println(res);
 
-    }
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        String s1 = in.nextLine();
-        String s2 = in.nextLine();
-        System.out.println(Solution(s1,s2));
     }
 }
