6426582d7636ed5ec7c7950e8e5c48007d8ba29c
Hoshi03 <ichanho114@gmail.com>
2024-12-01 19:21:45 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2428 ms, Memory: 359592 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,34 +1,18 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
-
 public class Main {
-
-    public static void solution(int[] arr) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int[] res = new int[10001];
-
-        for (int i =0; i < arr.length; i++){
-            res[arr[i]]++;
-        }
-
-        for (int i = 1; i < res.length; i++) {
-            while (res[i]-- > 0){
-                bw.write(i + "\n");
-            }
-        }
-        bw.flush();
-    }
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        Scanner in = new Scanner(System.in);
         int n = Integer.parseInt(br.readLine());
-        int[] arr= new int[n];
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(br.readLine());
-        solution(arr);
+        int[] arr = new int[n];
+        for (int i = 0; i < n ; i++) arr[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) sb.append(arr[i]).append('\n');
+        System.out.println(sb);
     }
 }
