94d54d0609b6f8da2945fb18255ed2b52de411a4
Hoshi03 <ichanho114@gmail.com>
2024-12-01 19:52:47 +0900

    [Silver III] Title: 두 수의 합, Time: 364 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,47 +1,33 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
-
 public class Main {
-
-    public static void solution(int[] arr, int sol) throws IOException {
-
-
-    }
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int[] arr= new int[n];
-        String s = br.readLine();
-        String[] num = s.split(" ");
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(num[i]);
-        int sol = Integer.parseInt(br.readLine());
-
-        Arrays.sort(arr);
-
-        int lt = 0;
-        int rt = arr.length-1;
-        int res = 0;
-
-        while (lt < rt){
-            if (arr[lt] + arr[rt] == sol) {
-                res++;
-                lt++;
-                rt--;
-            }
-            else if (arr[lt] + arr[rt] < sol) {
-                lt++;
-            }
-
-            else if (arr[lt] + arr[rt] > sol){
-                rt --;
-            }
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        System.out.println(res);
+        Arrays.sort(arr);
+        int target = Integer.parseInt(br.readLine());
+        int lt = 0, rt = n-1, cnt = 0;
+        while (lt < rt){
+            int tmp = arr[lt]+arr[rt];
+            if (tmp == target){
+                if (lt + 1 < rt)lt++;
+                else if (rt - 1 > lt) rt--;
+                else{
+                    rt++;
+                    lt++;
+                }
+                cnt++;
+            }
+            else if (tmp < target) lt++;
+            else rt--;
+        }
+        System.out.println(cnt);
     }
 }
