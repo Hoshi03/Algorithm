543f23f26038a6ada2d3ee2e484159b88fb25dd0
Hoshi03 <ichanho114@gmail.com>
2024-12-03 21:48:28 +0900

    [Bronze I] Title: 유레카 이론, Time: 172 ms, Memory: 20492 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -1,53 +1,36 @@
-import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
+import java.io.*;
 
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-public class Main {
+        int n = Integer.parseInt(br.readLine());
+        while (n --> 0){
+            int target = Integer.parseInt(br.readLine());
+            int cnt = 0;
+            boolean isTriangle = false;
+            int[] arr = new int[1001];
+            for(int i = 1; i <= 1000; i++){
+                int tmp = i * (i + 1) / 2;
+                if (tmp >= target) break;
+                arr[cnt++] = tmp;
+            }
 
-    public static ArrayList<Integer> solution(ArrayList<Integer> list, int[] arr )  {
-        ArrayList<Integer> res = new ArrayList<>();
-
-        for (int x : arr){
-
-            boolean is3 = false;
-            int tmp = 0;
-
-            for (int i =0; i < list.size(); i++){
-                tmp = list.get(i);
-                for (int j = 0; j < list.size(); j++){
-                    tmp = list.get(i) + list.get(j);
-                    for (int k =0; k < list.size(); k++){
-                        if (tmp + list.get(k) == x ) {
-                            is3 = true;
+            for (int i = 0; i < cnt; i++){
+                if (isTriangle) break;
+                for (int j = 0; j < cnt; j++){
+                    if (isTriangle) break;
+                    for (int k = 0; k < cnt; k++){
+                        if (arr[i] + arr[j] + arr[k] == target) {
+                            isTriangle = true;
                             break;
                         }
                     }
                 }
             }
-
-            if (is3) res.add(1);
-            else res.add(0);
-
+            if (!isTriangle) System.out.println(0);
+            else System.out.println(1);
         }
-
-        return res;
-    }
-
-    public static void main(String[] args) throws IOException {
-
-        Scanner in = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i =1; i <= 1000; i++){
-           if (i*(i+1)/2 <= 1000) list.add(i*(i+1)/2);
-           else break;
-        }
-
-        int n = in.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) arr[i] = in.nextInt();
-
-        for (int x : solution(list,arr)) System.out.println(x);
-
     }
 }
