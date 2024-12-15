4cab7518d4aee1511743fdb2cb931247289a0117
Hoshi03 <ichanho114@gmail.com>
2024-12-15 21:16:07 +0900

    [Silver II] Title: 사탕 게임, Time: 148 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -1,90 +1,74 @@
-import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
+import java.io.*;
 
+class Main {
+    static char[][] arr;
+    static int n;
 
-public class Main {
-
-    public static int solution(char[][] arr )  {
-        int res = 0;
-        int n = arr.length;
-
-        for (int i =0; i < n; i++){
-            for (int j =0; j < n; j++){
-                if (j+1 < n && arr[i][j] != arr[i][j+1]){
-                    swap(arr,i,j,i,j+1);
-                    res = Math.max(res, Math.max(findMaxCol(arr), findMaxLow(arr)));
-                    swap(arr,i,j,i,j+1);
-                }
-
-                if (i+1 < n && arr[i][j] != arr[i+1][j]){
-                    swap(arr,i,j,i+1,j);
-                    res = Math.max(res, Math.max(findMaxCol(arr), findMaxLow(arr)));
-                    swap(arr,i,j,i+1,j);
-                }
-            }
-        }
-
-        return res;
-    }
-
-    public static void swap(char[][] arr, int x1, int y1, int x2, int y2){
-        char tmp = arr[x1][y1];
-        arr[x1][y1] = arr[x2][y2];
-        arr[x2][y2] = tmp;
-    }
-
-    public static int findMaxLow(char[][] arr){
-        int max = 0;
-
-        for (int i =0; i < arr.length; i++){
-            int cnt = 1;
-            for (int j = 1; j < arr.length; j++){
-                if (arr[i][j] == arr[i][j-1]) cnt++;
-
+    public static int getMax() {
+        int maxCount = 0;
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            for (int j = 1; j < n; j++) {
+                if (arr[i][j] == arr[i][j - 1]) count++;
                 else {
-                    if (max < cnt) max = cnt;
-                    cnt = 1;
+                    maxCount = Math.max(maxCount, count);
+                    count = 1;
                 }
             }
-            max = Math.max(max,cnt);
+            maxCount = Math.max(maxCount, count);
         }
-        return max;
-    }
 
-    public static int findMaxCol(char[][] arr){
-        int max = 0;
-
-        for (int i =0; i < arr.length; i++){
-            int cnt = 1;
-            for (int j = 1; j < arr.length; j++){
-                if (arr[j][i] == arr[j-1][i]) cnt++;
-
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            for (int j = 1; j < n; j++) {
+                if (arr[j][i] == arr[j - 1][i]) count++;
                 else {
-                    if (max < cnt) max = cnt;
-                    cnt = 1;
+                    maxCount = Math.max(maxCount, count);
+                    count = 1;
                 }
             }
-            max = Math.max(max,cnt);
+            maxCount = Math.max(maxCount, count);
         }
-        return max;
-
+        return maxCount;
     }
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        in.nextLine();
-        char[][] arr = new char[n][n];
-        for (int i =0; i < n;i++){
-            String s = in.nextLine();
-            for (int j = 0; j < n; j++){
+        n = Integer.parseInt(br.readLine());
+        arr = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < n; j++) {
                 arr[i][j] = s.charAt(j);
             }
         }
 
-        System.out.println(solution(arr));
+        int res = getMax();
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j + 1 < n && arr[i][j] != arr[i][j + 1]) {
+                    swap(i, j, i, j + 1);
+                    res = Math.max(res, getMax());
+                    swap(i, j, i, j + 1); 
+                }
+
+                if (i + 1 < n && arr[i][j] != arr[i + 1][j]) {
+                    swap(i, j, i + 1, j);
+                    res = Math.max(res, getMax());
+                    swap(i, j, i + 1, j);
+                }
+            }
+        }
+        System.out.println(res);
+    }
+
+    public static void swap(int x1, int y1, int x2, int y2) {
+        char temp = arr[x1][y1];
+        arr[x1][y1] = arr[x2][y2];
+        arr[x2][y2] = temp;
     }
 }
