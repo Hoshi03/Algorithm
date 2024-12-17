02ca98c9d7a0663969581154178059148456f9f9
Hoshi03 <ichanho114@gmail.com>
2024-12-17 20:17:04 +0900

    [Silver V] Title: 단어 정렬, Time: 352 ms, Memory: 27188 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,31 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
-
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashSet<String> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++){
             set.add(br.readLine());
         }
 
-        ArrayList<String> arr = new ArrayList<>(set);
-        Collections.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if(o1.length() == o2.length()){
-                    return o1.compareTo(o2);
-                }
-                return o1.length() - o2.length();
+        ArrayList<String> arr = new ArrayList<>();
+
+        for (String x : set) arr.add(x);
+
+        arr.sort((a, b) -> {
+            if (a.length() == b.length()) {
+                return a.compareTo(b);
+            } else {
+                return a.length() - b.length();
             }
         });
-        for(var x : arr){
-            System.out.println(x);
+
+        for (String x : arr){
+            sb.append(x).append('\n');
         }
+
+        System.out.println(sb);
     }
 }
