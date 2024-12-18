e15c57ac2fcbb36104df6fd86e2892080d25b145
Hoshi03 <ichanho114@gmail.com>
2024-12-18 14:05:29 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1020 ms, Memory: 51920 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,22 +1,28 @@
 import java.util.*;
+import java.io.*;
+
+class People {
+
+}
 
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        TreeSet<String> res = new TreeSet<>();
-
-        for (int i = 0; i < n; i++){
-            String name = in.next();
-            String state = in.next();
-            if (state.equals("enter")) res.add(name);
-            else res.remove(name);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Map<String,String> mmap = new TreeMap<>();
+        while (n --> 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            mmap.put(st.nextToken(), st.nextToken());
         }
 
-        String[] result = res.toArray(res.toArray(new String[res.size()]));
-        Arrays.sort(result, Collections.reverseOrder());
+        ArrayList<String> arr = new ArrayList<>();
 
-        for (String x : result) System.out.println(x);
+        for (String x : mmap.keySet()){
+            if (mmap.get(x).equals("enter")) arr.add(x);
+        }
+
+        Collections.sort(arr,Collections.reverseOrder());
+
+        for (String x : arr) System.out.println(x);
     }
 }
