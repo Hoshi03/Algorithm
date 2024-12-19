649abfe82ea91d9a02e71f2f2af9da2ee5420374
Hoshi03 <ichanho114@gmail.com>
2024-12-19 18:18:23 +0900

    [Silver IV] Title: 베스트셀러, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -1,30 +1,37 @@
 import java.util.*;
+import java.io.*;
 
-class Main
-{
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
+class Book{
+    String name;
+    int count;
+    public Book(String name, int count){
+        this.name = name;
+        this.count = count;
+    }
+}
 
-        int N = sc.nextInt();
-        String[] titles = new String[N];
-        for (int i = 0; i < N; i++)
-            titles[i] = sc.next();
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Map<String, Integer> mmap = new HashMap<>();
 
-        Arrays.sort(titles);
-
-        String maxTitle = titles[0];
-        int maxCount = 1;
-        int currentCount = 1;
-        for (int i = 1; i < N; i++) {
-            if (!titles[i].equals(titles[i - 1]))
-                currentCount = 0;
-            currentCount++;
-            if (maxCount < currentCount ||
-                    (maxCount == currentCount && titles[i].compareTo(maxTitle) < 0)) {
-                maxCount = currentCount;
-                maxTitle = titles[i];
-            }
+        while (n --> 0){
+            String s = br.readLine();
+            mmap.put(s, mmap.getOrDefault(s,0)+1);
         }
-        System.out.println(maxTitle);
+        ArrayList<Book> arr = new ArrayList<>();
+        for (String x : mmap.keySet()){
+            arr.add(new Book(x, mmap.get(x)));
+        }
+
+        arr.sort((a,b) -> {
+           if (a.count == b.count){
+               return a.name.compareTo(b.name);
+           }
+           else return b.count - a.count;
+        });
+
+        System.out.println(arr.get(0).name);
     }
 }
