0b363ac94908b387cf07cde04224e82a9ef23ab8
Hoshi03 <ichanho114@gmail.com>
2024-12-19 18:37:19 +0900

    [Silver II] Title: 좌표 압축, Time: 2052 ms, Memory: 277340 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,35 +1,39 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main
-{
-    public static void main (String[] args) throws IOException {
+        /*
+        일단 입력받은걸 arr에 순차적으로 key형태로 저장
+        받은걸 set에 저장
+        map<int, int>에 정렬된걸 넣으면서 순차적으로 0 1 2 3 4 식으로 증가
+        map.get(arr[i]) 형태로 가져온걸 stringbuilder에 저장
+        stringbuilder 출력
+         */
 
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-       int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        Set<Integer> set = new TreeSet<>();
+        Map<Integer, Integer> mmap = new HashMap<>();
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++){
+            int tmp = Integer.parseInt(st.nextToken());
+            arr[i] = tmp;
+            set.add(tmp);
+        }
 
-       String[] input =  br.readLine().split(" ");
-
-        //중복 제거해서 정렬할 트리맵
-       TreeMap<Long, Integer> treeMap = new TreeMap<>();
-
-       for (int i =0; i < n; i++){
-           treeMap.put(Long.parseLong(input[i]),0);
-       }
-
-        int idx = 0;
-
-       //트리맵 순회하면서 밸류에 자기보다 작은 숫자 갯수를 넣어둠
-        for (Map.Entry<Long, Integer> x : treeMap.entrySet()){
-            Long x1 = x.getKey();
-            treeMap.put(x1,idx++);
+        int tmp = 0;
+        for (int x : set){
+            mmap.put(x, tmp++);
         }
 
         for (int i = 0; i < n; i++){
-            bw.write(treeMap.get(Long.parseLong(input[i])) + " ");
+            sb.append(mmap.get(arr[i])).append(" ");
         }
-        bw.flush();
+
+        System.out.println(sb);
     }
 }
